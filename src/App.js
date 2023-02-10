c06995120a0ef1a55c49305ab6ef63f7dfa08871
Annie <annielin28815@gmail.com>
2023-02-11 00:56:47 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link, Switch, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Link to="/home">Home</Link>
+      </div>
+      <div>
+        <Link to="/login">login</Link>
+      </div>
+      <div>
+        <Link to="/member">member center</Link>
+      </div>
+      <div>
+        <Link to="/orders">orders</Link>
+      </div>
+
+      <Switch>
+        <Route path="/home">Home</Route>
+        <Route path="/login">login</Route>
+        <Route path="/member">member center</Route>
+        <Route path="/orders">orders</Route>
+        <Route path="/step/1">Step 1</Route>
+        <Route path="/step/2">Step 2</Route>
+        <Route path="/step/3">Step 3</Route>
+        <Route path="/step/4">Step 4</Route>
+        <Route path="/step/5">Step 5</Route>
+        {/* <Route path="*">404 page</Route> */}
+      </Switch>
+    </BrowserRouter>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>Reservation System</p>
+    //   </header>
+    // </div>
   );
 }
 
